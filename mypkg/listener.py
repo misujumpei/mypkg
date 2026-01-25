5bda6ffc8ce0b760c5f943780e8d38fa6e635965
misujumpei <misujumpei@gmail.com>
2026-01-25 14:17:33 +0900

    listenerにライブラリを追加

---
+++ mypkg/listener.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2026 misujumpei
+# SPDX-License-Identifier: BSD-3-Clause
+
+import rclpy                     # ROS 2の基本ライブラリ
+from rclpy.node import Node      # rclpyの中からNodeを取り出す
+from std_msgs.msg import String   # 標準的なメッセージの型を読み込む
