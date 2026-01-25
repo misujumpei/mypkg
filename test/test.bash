b1f0a5eb4cc1c5425d4393fb56a1ffe31e09f997
misujumpei <misujumpei@gmail.com>
2026-01-25 17:33:09 +0900

    minimal test script

--- test/test.bash
+++ test/test.bash
@@ -3,21 +3,18 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
-[ "$1" != "" ] && dir="$1"
+[ "$1" != "" ] && dir="$1" 
 
-# ビルド
 cd $dir/ros2_ws
 colcon build
+source /opt/ros/humble/setup.bash # 環境読み込み
 source install/setup.bash
 
-# 環境変数をセット
-export PYTHONUNBUFFERED=1
+# 10秒動かしてログへ代入。&で裏で動かす
+timeout 10 ros2 run mypkg talker > /tmp/mypkg.log 2>&1 &
+timeout 10 ros2 run mypkg listener >> /tmp/mypkg.log 2>&1 &
 
-# 10秒だけtalkerを動かす。bash -c を使って設定を確実に読み込ませる
-timeout 10 bash -c "source /opt/ros/humble/setup.bash; source $dir/ros2_ws/install/setup.bash; ros2 run mypkg talker" > /tmp/talker.log 2>&1
+sleep 5 # 通信を待つ
 
-# ログ確認用
-cat /tmp/talker.log
-
-# 答えが入ってれば合格
-grep '答え' /tmp/talker.log
+# 判定
+cat /tmp/mypkg.log | grep '答え'
