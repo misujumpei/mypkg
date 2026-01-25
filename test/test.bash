7f5b39fef6a47934fe1fe9d27140759a6475b906
misujumpei <misujumpei@gmail.com>
2026-01-25 17:19:51 +0900

    minimal test

--- test/test.bash
+++ test/test.bash
@@ -5,15 +5,17 @@
 dir=~
 [ "$1" != "" ] && dir="$1"
 
+# ビルド
 cd $dir/ros2_ws
 colcon build
 source install/setup.bash
 
-# talkerを動かしてログを保存
-ros2 run mypkg talker --once > /tmp/talker.log 2>&1
+# 5秒talker動かしてログに保存
+# 設定読み込み
+timeout 5 bash -c "source /opt/ros/humble/setup.bash; source $dir/ros2_ws/install/setup.bash; export PYTHONUNBUFFERED=1; ros2 run mypkg talker" > /tmp/talker.log 2>&1
 
 # ログ確認
 cat /tmp/talker.log
 
-# 答えが含まれてるか判定
+# 判定
 grep '答え' /tmp/talker.log
