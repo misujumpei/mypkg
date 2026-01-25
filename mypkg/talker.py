8f9dacc9dd47e6c228db3aa7b04bca9f6a7f8673
misujumpei <misujumpei@gmail.com>
2026-01-25 13:21:33 +0900

    シバンとライブラリを入力

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,6 +1,8 @@
-def main():
-    print('Hi from mypkg.')
+#!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2026 misujumpei
+# SPDX-License-Identifier: BSD-3-Clause
 
-
-if __name__ == '__main__':
-    main()
+import rclpy                     # ROS 2の基本ライブラリ
+from rclpy.node import Node      # rclpyの中からNodeを取り出す
+from std_msgs.msg import String  # 標準入力から文字列を読み込む
+import random                    # 乱数を生成する
