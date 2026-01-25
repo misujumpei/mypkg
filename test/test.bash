24de13fa12c238ec1ded6d1bf6a7e2a7bafa3209
misujumpei <misujumpei@gmail.com>
2026-01-25 17:14:58 +0900

    testをシンプル化

--- test/test.bash
+++ test/test.bash
@@ -5,24 +5,15 @@
 dir=~
 [ "$1" != "" ] && dir="$1"
 
-cd $dir/ros2_ws  # ビルドとセットアップ
-colcon build     # パッケージを作る 
+cd $dir/ros2_ws
+colcon build
 source install/setup.bash
 
-# 出力を溜め込まず、すぐにファイルに書き込む設定
-export PYTHONUNBUFFERED=1
+# talkerを動かしてログを保存
+ros2 run mypkg talker --once > /tmp/talker.log 2>&1
 
-# talker, listener は10秒経ったら強制終了する、ROS2の設定を読み込んでから実行するように
-timeout 10 bash -c "source /opt/ros/humble/setup.bash; source $dir/ros2_ws/install/setup.bash; ros2 run mypkg talker" > /tmp/talker.log 2>&1 &
-timeout 10 bash -c "source /opt/ros/humble/setup.bash; source $dir/ros2_ws/install/setup.bash; ros2 run mypkg listener" > /tmp/listener.log 2>&1 &
+# ログ確認
+cat /tmp/talker.log
 
-# 通信してメッセージがでるまで5秒待つ
-sleep 5
-
-# デバッグ用にログの中身を表示
-echo "=== Listener Log Content ==="
-cat /tmp/listener.log
-echo "============================"
-
-# listenerに 答え と出たら終了
-grep '答え:' /tmp/listener.log
+# 答えが含まれてるか判定
+grep '答え' /tmp/talker.log
