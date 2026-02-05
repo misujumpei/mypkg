f46801ecaec91067f749a40f870e9fddb8da54e1
misujumpei <misujumpei@gmail.com>
2026-02-05 22:16:46 +0900

    testを1つずつ確認するように修正2

--- test/test.bash
+++ test/test.bash
@@ -3,5 +3,10 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 # 1. 環境の読み込みチェック
-echo "--- Step 1: ROS 2の読み込み ---"
+echo "--- ROS 2の読み込み ---"
 source /opt/ros/humble/setup.bash && echo "ROS 2 OK"
+
+# 2. ビルドができるかチェック
+echo "--- ビルド実行 ---"
+cd /root/ros2_ws
+colcon build --packages-select mypkg && echo "Build OK!"
