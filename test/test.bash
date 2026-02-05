f3ac54e236c37794be9ba0a5edd9b64475205f48
misujumpei <misujumpei@gmail.com>
2026-02-05 22:25:03 +0900

    testを1つずつ確認するように修正3

--- test/test.bash
+++ test/test.bash
@@ -2,11 +2,17 @@
 # SPDX-FileCopyrightText: 2026 misujumpei
 # SPDX-License-Identifier: BSD-3-Clause
 
-# 1. 環境の読み込みチェック
+
 echo "--- ROS 2の読み込み ---"
 source /opt/ros/humble/setup.bash && echo "ROS 2 OK"
 
-# 2. ビルドができるかチェック
+
 echo "--- ビルド実行 ---"
 cd /root/ros2_ws
-colcon build --packages-select mypkg && echo "Build OK!"
+colcon build --packages-select mypkg && echo "Build OK"
+
+echo "--- プログラムの存在確認 ---"
+# 設定を読み込む
+source install/setup.bash
+# ファイルがあるか確認して、あればメッセージを出す
+ls install/mypkg/lib/mypkg/talker && echo "Program File OK"
