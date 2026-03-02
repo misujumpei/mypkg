e93eb55803c00a8f38c7a827744df8eac566072f
misujumpei <misujumpei@gmail.com>
2026-03-02 17:40:52 +0900

    GitHub ActionsでのROSログフォルダ作成エラーを回避

--- test/test.bash
+++ test/test.bash
@@ -9,10 +9,12 @@ cd /root/ros2_ws
 colcon build --packages-select mypkg
 source install/setup.bash
 
+export ROS_LOG_DIR=/tmp/ros_log
+mkdir -p /tmp/ros_log
 
 export PYTHONUNBUFFERED=1
 
-# ノードを15秒間動かす
+# 15秒間動かす
 timeout 15 ros2 run mypkg talker > /tmp/talker.log 2>&1 &
 timeout 15 ros2 run mypkg listener > /tmp/listener.log 2>&1 &
 
